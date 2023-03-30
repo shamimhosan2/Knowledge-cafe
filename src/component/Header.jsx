5e380192e15b8274d05d59eabef79e3eadd9cfd5
Md Shamim Hosan <mdshamimhosan77@gmail.com>
2023-03-30 22:30:32 +0600

    fast chacj git

---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='container-header'>
+            <h2>Knowledge Cafe</h2>
+        </div>
+    );
+};
+
+export default Header;
